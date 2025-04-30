bf104b78359b46fee994dd04cc8d2a7ba0903ebd
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2025-04-30 18:00:40 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -2,9 +2,9 @@
 #setup for aws linux
 sudo yum update -y
 sudo yum install java-17 -y
-cd /tmp
+cd /opt
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip
-sudo unzip  sonarqube-9.9.0.65466.zip -d /opt
+sudo unzip  sonarqube-9.9.0.65466.zip 
 sudo useradd sonar
 sudo echo redhat | passwd --stdin sonar
 sudo mv /opt/sonarqube-9.9.0.65466    sonarqube
