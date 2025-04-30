1fb0c7ce1bc89a96d018953359b87b3accfb1e01
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2025-04-30 17:54:35 +0530

    Create sonarqube.sh

---
+++ sonarqube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#setup for aws linux
+sudo yum update -y
+sudo yum install java-17 -y
+cd /tmp
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip
+sudo unzip  sonarqube-9.9.0.65466.zip -d /opt
+sudo useradd sonar
+sudo echo redhat | passwd --stdin sonar
+sudo mv /opt/sonarqube-9.9.0.65466    sonarqube
+sudo chown  sonar:sonar  /opt/sonarqube  -R
+sudo usermod  -d /opt/sonarqube  sonar
+
+#switch to the sonar user 
+# su - sonar
+# cd bin/linux-x86-64
+#sh sonar.sh  start 
+#sh sonar.sh status 
